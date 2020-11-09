b75e9b3e25263036cf5d20472d771cacda18f4f4
dianamukhametgalina <71024367+dianamukhametgalina@users.noreply.github.com>
2020-11-09 21:24:30 +0300

    Задание выполнено

---
+++ HomeWork4/HomeWork4/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4_1
+{
+    class Program
+    {
+        static int Check(ref string str)  //Проверка на десятичное число, замена точки на запятую
+        {
+            int num;
+
+            if (!int.TryParse(str, out num))
+            {
+                Console.WriteLine("Необходимо ввести число! Конец программы.");
+                Console.WriteLine("Для выхода нажмите любую кнопку...");
+                Console.ReadKey();
+                Environment.Exit(37707);
+            }
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            {
+                double[] arr = new double[1];
+                Console.WriteLine("Введите количество элементов массива:");
+                string str = Console.ReadLine();
+                int N = Check(ref str);
+                if (N <= 0)
+                {
+                    Console.WriteLine("Массива не существует");
+                    Console.ReadKey();
+                    Environment.Exit(404);
+                }
+                Array.Resize(ref arr, N);
+
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Массив вещественных чисел:");
+                Random rnd = new Random();
+
+                for (int i = 0; i < N; i++)
+                {
+                    arr[i] = rnd.Next(-10, 10) + rnd.NextDouble();
+                    Console.WriteLine("{0:N2}", arr[i]);
+                }
+                Console.WriteLine();
+                double min = Math.Abs(arr[0]);
+                for (int i = 0; i < N; i++)
+                    if (Math.Abs(arr[i]) <= Math.Abs(min))
+                        min = arr[i];
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Минимальный по модулю элемент:  {0:N2}", min);
+                int x = 0;
+                bool c = false;
+                for (int i = 0; i < N; i++)
+                    if (arr[i] == 0)
+                    {
+                        x = i;
+                        c = true;
+                        break;
+                    }
+                if (c)
+                {
+                    double sum = 0;
+                    for (int i = x + 1; i < N; i++)
+                        sum += arr[i];
+                    Console.WriteLine("Сумма элементов расположенных после первого нулевого: {0:N2}", sum);
+                }
+                else
+                    Console.WriteLine("В массиве нет нулевых элементов.");
+
+                double[] f = new double[N];
+                for (int i = 0; i < N; i++)
+                    f[i] = arr[i];
+                int g = -1;
+                for (int i = 0; i < N; i++)
+                    if (i % 2 == 1)
+                    {
+                        g++;
+                        arr[g] = f[i];
+                    }
+                for (int i = 0; i < N; i++)
+                    if (i % 2 == 0)
+                    {
+                        g++;
+                        arr[g] = f[i];
+                    }
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Преобразованный массив: ");
+                for (int i = 0; i < N; i++)
+                    Console.WriteLine("{0:N2}", arr[i]);
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Для выхода нажмите любую кнопку...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
---
+++ HomeWork4/HomeWork4_2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Целочисленная матрица размером 10х10:");
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Random rnd = new Random();
+            int[,] Array = new int[10, 10];
+
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    Array[y, x] = rnd.Next(-100, 100);
+                }
+            }
+
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    Console.Write(Array[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = i + 1; j < 10; j++)
+                {
+                    sum += Math.Abs(Array[i, j]);
+                }
+            }
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine("Сумма модулей элементов, расположенных выше главной диагонали: {0}", sum);
+
+            int Localmin = 0;
+            for (int i = 1; i < Array.GetLength(0) - 1; i++)
+                for (int j = 1; j < Array.GetLength(1) - 1; j++)
+                {
+                    if ((Array[i, j] < Array[i - 1, j - 1])
+                    && (Array[i, j] < Array[i - 1, j])
+                    && (Array[i, j] < Array[i - 1, j + 1])
+                    && (Array[i, j] < Array[i, j - 1])
+                    && (Array[i, j] < Array[i, j + 1])
+                    && (Array[i, j] < Array[i + 1, j - 1])
+                    && (Array[i, j] < Array[i + 1, j])
+                    && (Array[i, j] < Array[i + 1, j + 1]))
+                    {
+                        Localmin++;
+                    }
+
+                }
+            Console.WriteLine(" ");
+            Console.WriteLine("Количество локальных минимумов на заданной матрице: " + Localmin);
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("Для выхода нажмите любую кнопку...");
+            Console.ReadKey();
+        }
+    }
+
+}
+
